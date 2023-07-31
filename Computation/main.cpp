590ebc67ca036de31ecf0ec9da4b3e12c6670ffc
Esteban Delaunay <82404669+Regeraa@users.noreply.github.com>
2023-07-31 10:57:09 +0200

    Initialisation of some variable

--- Computation/main.cpp
+++ Computation/main.cpp
@@ -1,6 +1,38 @@
+#include <iostream>
 
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
 
 int main() 
 {
+	// Paramètres physiques
+	constexpr double radius(0.5);
+	constexpr double Re(200);
+	constexpr double pi(3.14159);
 
+	// Maillage espace
+	constexpr int sizeR(100);
+	constexpr int sizeTheta(150);
+
+	// Paramètres de calcul
+	constexpr int kMax(1e3);
+	constexpr double eps(1e-5);
+
+	// Maillage en coordonnée polaire
+	constexpr double lengthR = 19.0 * radius;
+	constexpr double stepSizeR = lengthR / (sizeR - 1.0);
+	double r[sizeR]{0.0};
+	for (double* p = r+1; p != end(r); p++) { *p = *(p - 1) + stepSizeR; }
+
+	constexpr double lengthTheta = 2.0 * pi;
+	constexpr double stepSizeTheta = lengthTheta / (sizeTheta - 1.0);
+	double theta[sizeTheta]{ 0.0 };
+	for (double* p = theta + 1; p != end(theta); p++) { *p = *(p - 1) + stepSizeTheta; }
+
+
+	cout << "Done" << endl;
+
+	return 0;
 }
