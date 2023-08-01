03a1c8f1097c5e02bfbcdc8d976591cb36ab8451
Esteban Delaunay <82404669+Regeraa@users.noreply.github.com>
2023-08-01 13:14:33 +0200

    Adding vorticity

--- Computation/main.cpp
+++ Computation/main.cpp
@@ -1,73 +1,124 @@
 #include <cmath>
 #include <fstream>
 
+using std::pow;
+
 int main()
 {
+	// Ouverture du fichier pour export des données
+	std::ofstream expFile("data", std::ofstream::out);
+
 	// Paramètres physiques
 	constexpr double radius = 0.5, Re = 200.0, uInf = 1.0;
 	constexpr double pi(3.141592653589793);
 
+	// Paramètres temps
+	int timeCurrent(0);
+	constexpr int timeMax(10000);
+	const double stepSizeTime(1e-2), & dt = stepSizeTime;
+
 	// Maillage espace
-	constexpr size_t sizeR(40), sizeTheta(40);
+	constexpr size_t sizeR(50), sizeTheta(75);
 
 	// Paramètres de calcul
-	constexpr int kMax(100000);
-	constexpr double eps(1e-5);
+	constexpr int kMax(10000);
+	constexpr double eps(1e-5), om_psi(1.9);
 
 	// Maillage en coordonnée polaire
 	constexpr double lengthR = 19.0 * radius;
-	constexpr double stepSizeR = lengthR / (sizeR - 1.0);
+	const double stepSizeR = lengthR / (sizeR - 1.0), & dr = stepSizeR;
 	double r[sizeR]{ radius };
 	for (double* p = r + 1; p != std::end(r); p++) { *p = *(p - 1) + stepSizeR; }
 
 	constexpr double lengthTheta = 2.0 * pi;
-	constexpr double stepSizeTheta = lengthTheta / (sizeTheta - 1.0);
+	const double stepSizeTheta = lengthTheta / (sizeTheta - 1.0), & dth = stepSizeTheta;
 	double theta[sizeTheta]{ 0.0 };
 	for (double* p = theta + 1; p != std::end(theta); p++) { *p = *(p - 1) + stepSizeTheta; }
 
+	// Initialisation des matrices
 	double psi[sizeR][sizeTheta]{ 0.0 };
 	double psiOld[sizeR][sizeTheta]{ 0.0 };
 
+	double w[sizeR][sizeTheta]{ 0.0 };
+	double wOld[sizeR][sizeTheta]{ 0.0 };
+
 	// Conditions aux bords
-	for (size_t i = 0; i != sizeR; ++i) {
-		psi[i][0] = 20.0;
-		psi[i][sizeTheta - 1] = 20.0;
+	for (size_t j = 0; j != sizeTheta; ++j) {
+		psi[sizeR-1][j] = r[sizeR-1] * std::sin(theta[j]);
 	}
 
-	for (size_t j = 0; j != sizeTheta; ++j) {
-		psi[0][j] = 20;
-		psi[sizeR - 1][j] = uInf * 19.0 * radius * sin(theta[j]) + 20.0;
+	// Initialisation des constantes de calcul
+	const double dr2 = pow(dr, 2), dth2 = pow(dth, 2);
+	double g(0.0), f(0.0);
+
+	double a[sizeR]{}, b[sizeR]{}, c[sizeR]{}, d[sizeR]{};
+	for (size_t i = 0; i != sizeR; i++) {
+		a[i] = 2.0 * (1.0 / dr2 + 1.0 / (pow(r[i],2.0) * dth2));
+		b[i] = 1.0 / dr2 + 1.0 / (2.0 * r[i] * dr);
+		c[i] = 1.0 / dr2 - 1.0 / (2.0 * r[i] * dr);
+		d[i] = 1.0 / (pow(r[i], 2.0) * dth2);
 	}
 
 	// Calcul
-	int k = 0;
-	double error = 1.0;
-	while (k < kMax && error > eps)
-	{
+	while (timeCurrent != timeMax) {
+		timeCurrent++;
+		
 		for (size_t i = 0; i != sizeR; ++i) {
 			for (size_t j = 0; j != sizeTheta; ++j) {
-				psiOld[i][j] = psi[i][j];
+				wOld[i][j] = w[i][j];
 			}
 		}
 
-		for (size_t i = 1; i != sizeR-1; ++i) {
-			for (size_t j = 1; j != sizeTheta-1; ++j) {
-				psi[i][j] = ((1.0 / (stepSizeR * stepSizeR) + 1.0 / (2.0 * r[i] * stepSizeR)) * psiOld[i + 1][j] + (1.0 / (stepSizeR * stepSizeR) - 1.0 / (2.0 * r[i] * stepSizeR)) * psiOld[i - 1][j] + (psiOld[i][j + 1] + psiOld[i][j - 1]) / ((r[i] * r[i]) * (stepSizeTheta * stepSizeTheta))) * 1.0 / (2.0 / (stepSizeR * stepSizeR) + 2.0 / ((r[i] * r[i]) * (stepSizeTheta * stepSizeTheta)));
+		int k = 0;
+		double error = 1.0;
+		while (k != kMax && error > eps)
+		{
+			for (size_t i = 0; i != sizeR; ++i) {
+				for (size_t j = 0; j != sizeTheta; ++j) {
+					psiOld[i][j] = psi[i][j];
+				}
 			}
-			psi[i][0] =     ((1.0 / (stepSizeR * stepSizeR) + 1.0 / (2.0 * r[i] * stepSizeR)) * psiOld[i + 1][0] + (1.0 / (stepSizeR * stepSizeR) - 1.0 / (2.0 * r[i] * stepSizeR)) * psiOld[i - 1][0] + (psiOld[i][1] + psiOld[i][sizeR - 2]) / ((r[i] * r[i]) * (stepSizeTheta * stepSizeTheta))) * 1.0 / (2.0 / (stepSizeR * stepSizeR) + 2.0 / ((r[i] * r[i]) * (stepSizeTheta * stepSizeTheta)));
-			psi[i][sizeTheta-1] = psi[i][0];
+
+			for (size_t i = 1; i != sizeR - 1; ++i) {
+				for (size_t j = 1; j != sizeTheta - 1; ++j) {
+					psi[i][j] = (1.0 - om_psi) * psiOld[i][j] + (om_psi / a[i]) * (b[i] * psi[i + 1][j] + c[i] * psi[i - 1][j] + d[i] * (psi[i][j + 1] + psi[i][j - 1]        ) + w[i][j]);
+				}
+				psi[i][0] =     (1.0 - om_psi) * psiOld[i][0] + (om_psi / a[i]) * (b[i] * psi[i + 1][0] + c[i] * psi[i - 1][0] + d[i] * (psi[i][1]     + psi[i][sizeTheta - 2]) + w[i][0]);
+				psi[i][sizeTheta - 1] = psi[i][0];
+
+			}
+
+			error = 0.0;
+			for (size_t i = 0; i != sizeR; ++i) {
+				for (size_t j = 0; j != sizeTheta; ++j) {
+					error += psi[i][j] - psiOld[i][j];
+				}
+			}
 		}
 
-		error = 0.0;
-		for (size_t i = 0; i != sizeR; ++i) {
-			for (size_t j = 0; j != sizeTheta; ++j) {
-				error += psi[i][j] - psiOld[i][j];
-			}
+		
+				
+		for (size_t j = 0; j != sizeTheta; ++j) {
+			w[0][j] = (psi[2][j] - 8.0 * psi[1][j]) / (2.0 * dr2);
 		}
+
+		for (size_t i = 1; i != sizeR - 1; ++i) {
+			for (size_t j = 1; j != sizeTheta - 1; ++j) {
+				g = (psi[i + 1][j] - psi[i - 1][j]) * (w[i][j + 1] - w[i][j - 1]) - (psi[i][j + 1] - psi[i][j - 1]) * (w[i + 1][j] - w[i - 1][j]);
+				f = g / (4.0 * r[i] * dr * dth) + (2.0 / Re) * (b[i] * w[i + 1][j] + c[i] * w[i - 1][j] + d[i] * (w[i][j + 1] + w[i][j - 1]) - a[i] * w[i][j]);
+				w[i][j] = wOld[i][j] + dt * f;
+			}
+
+			g = (psi[i + 1][0] - psi[i - 1][0]) * (w[i][1] - w[i][sizeTheta - 2]) - (psi[i][1] - psi[i][sizeTheta - 2]) * (w[i + 1][0] - w[i - 1][0]);
+			f = g / (4.0 * r[i] * dr * dth) + (2.0 / Re) * (b[i] * w[i + 1][0] + c[i] * w[i - 1][0] + d[i] * (w[i][1] + w[i][sizeTheta - 2]) - a[i] * w[i][0]);
+			w[i][0] = wOld[i][0] + dt * f;
+			w[i][sizeTheta - 1] = w[i][0];
+
+		}
+		
 	}
 
 	// Exportation des données
-	std::ofstream expFile("data", std::ofstream::out);
 	for (size_t i = 0; i != sizeR; ++i) {
 		for (size_t j = 0; j != sizeTheta; ++j) {
 			expFile << psi[i][j] << " ";
--- main.py
+++ main.py
@@ -1,62 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-psi = np.loadtxt("Computation\data")
+if __name__ == "__main__":
+    psi = np.loadtxt("Computation\data")
 
-# Paramètres physiques
-radius = 0.5
-Re = 200.0
-uInf = 1.0
+    # Paramètres physiques
+    radius = 0.5
 
-# Maillage espace
-sizeR = 40
-sizeTheta = 40
+    # Maillage espace
+    sizeR = 50
+    sizeTheta = 75
 
-# Maillage en coordonnée polaire
-lengthR = 19.0 * radius
-stepSizeR = lengthR / (sizeR - 1.0)
-r = np.linspace(radius, radius + lengthR, sizeR)
+    # Maillage en coordonnée polaire
+    lengthR = 19.0 * radius
+    stepSizeR = lengthR / (sizeR - 1.0)
+    r = np.linspace(radius, radius + lengthR, sizeR)
 
-lengthTheta = 2.0 * np.pi
-stepSizeTheta = lengthTheta / (sizeTheta - 1.0)
-theta = np.linspace(0, lengthTheta, sizeTheta)
+    lengthTheta = 2.0 * np.pi
+    stepSizeTheta = lengthTheta / (sizeTheta - 1.0)
+    theta = np.linspace(0, lengthTheta, sizeTheta)
 
-# Maillage en coordonnée cartésienne
-x = np.zeros([sizeR, sizeTheta])
-y = np.zeros([sizeR, sizeTheta])
-for i in range(sizeR):
-    for j in range(sizeTheta):
-        x[i, j] = r[i] * np.cos(theta[j])
-        y[i, j] = r[i] * np.sin(theta[j])
-        
-# APPROXIMATION DES VITESSES PAR DIFFERENCES FINIES
-# CALCUL DE UR
-ur = np.zeros([sizeR, sizeTheta])
-for i in range(0, sizeR):
-    for j in range(1, sizeTheta-1):
-        ur[i, j] =   (psi[i, j+1]         - psi[i, j-1]        ) / (2*r[i]*stepSizeTheta)
-    ur[i, 0] =       (psi[i, 1]           - psi[i, 0]          ) / (r[i]*stepSizeTheta)
-    ur[i, sizeR-1] = (psi[i, sizeTheta-1] - psi[i, sizeTheta-2]) / (r[i]*stepSizeTheta)
+    # Maillage en coordonnée cartésienne
+    x = np.zeros([sizeR, sizeTheta])
+    y = np.zeros([sizeR, sizeTheta])
+    for i in range(sizeR):
+        for j in range(sizeTheta):
+            x[i, j] = r[i] * np.cos(theta[j])
+            y[i, j] = r[i] * np.sin(theta[j])
 
+    # APPROXIMATION DES VITESSES PAR DIFFERENCES FINIES
+    # CALCUL DE UR
+    ur = np.zeros([sizeR, sizeTheta])
+    for i in range(0, sizeR):
+        for j in range(1, sizeTheta-1):
+            ur[i, j] = (psi[i, j+1] - psi[i, j-1]) / (2*r[i]*stepSizeTheta)
+        ur[i, 0] = (psi[i, 1] - psi[i, 0]) / (r[i]*stepSizeTheta)
+        ur[i, sizeTheta-1] = ur[i, 0]
 
-# CALCUL DE UT
-ut = np.zeros([sizeR, sizeTheta])
-for j in range(0, sizeTheta):
-    for i in range(1, sizeR-1):
-        ut[i, j] =   - (psi[i+1, j]     - psi[i-1, j]    ) / (2*stepSizeR)
-    ut[0, j] =       - (psi[1, j]       - psi[0, j]      ) / stepSizeR
-    ut[sizeR-1, j] = - (psi[sizeR-1, j] - psi[sizeR-2, j]) / stepSizeR
+    # CALCUL DE UT
+    ut = np.zeros([sizeR, sizeTheta])
+    for j in range(0, sizeTheta):
+        for i in range(1, sizeR-1):
+            ut[i, j] = - (psi[i+1, j] - psi[i-1, j]) / (2*stepSizeR)
+        ut[0, j] = - (psi[1, j] - psi[0, j]) / stepSizeR
+        ut[sizeR-1, j] = - (psi[sizeR-1, j] - psi[sizeR-2, j]) / stepSizeR
 
-# PROJECTION DANS LA BASE CARTESIENNE
-ux = ur * np.cos(theta) - ut * np.sin(theta)
-uy = ur * np.sin(theta) + ut * np.cos(theta)
-        
+    # PROJECTION DANS LA BASE CARTESIENNE
+    ux = ur * np.cos(theta) - ut * np.sin(theta)
+    uy = ur * np.sin(theta) + ut * np.cos(theta)
 
-# CALCUL DE LA NORME DE LA VITESSE
-u = ux**2 + uy**2
+    # CALCUL DE LA NORME DE LA VITESSE
+    u = np.sqrt(ux**2 + uy**2)
 
-
-fig, ax = plt.subplots()
-ax.pcolormesh(x, y, u, shading="nearest")
-ax.axis("equal")
-plt.show()
+    fig, ax = plt.subplots()
+    ax.pcolormesh(x, y, u, shading="gouraud")
+    ax.axis("equal")
+    plt.show()
